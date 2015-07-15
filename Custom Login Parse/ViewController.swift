97a0deaaf13a13deac70c3f7ce044063ba82cea9
asketola <annemarie.s.ketola@gmail.com>
2015-07-15 00:33:13 -0700

    Initial Commit

---
+++ Custom Login Parse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom Login Parse
+//
+//  Created by Annemarie Ketola on 7/15/15.
+//  Copyright (c) 2015 Annemarie Ketola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
